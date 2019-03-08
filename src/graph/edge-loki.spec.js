978b12a10b2639fcff84cf55498a3d9ea72bb13e
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-03-07 19:00:38 -0800

    Fix unit test

--- src/graph/edge-loki.spec.js
+++ src/graph/edge-loki.spec.js
@@ -10,6 +10,6 @@ describe.only('edge-loki', () => {
   it('WRITE EDGE', () => {
     const system = build('edge');
     const edge = system(E(WRITE, EDGE), [WRITE, EDGE]);
-    b64(edge).should.be.false;
+    b64(edge).should.equal("ODU3RwsJsF9v2EddUOxOCixFGzOJ3fsEueULjRFa9dE=");
   });
 });
