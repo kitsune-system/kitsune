b7bc0870bcdcc45bf1ba36652fa06e01196099af
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-05-12 14:05:19 -0700

    Delete "app" directory

--- src/app/misc.js
+++
@@ -1,9 +0,0 @@
-import { e, random } from './hash';
-import { READ, RANDOM } from './nodes';
-import System from './system';
-
-const Misc = System();
-
-Misc.command(e(READ, RANDOM), random);
-
-export default Misc;
