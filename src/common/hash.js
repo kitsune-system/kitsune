bb5a852ed79ffe133f4e6bb6f4412c53f70a920e
Edward Nicholes Jr <GameDevFox@gmail.com>
2019-05-14 22:14:16 -0700

    Be able to create hash of an exact size, down to the bit

--- src/common/hash.js
+++ src/common/hash.js
@@ -7,7 +7,53 @@ const sha256 = sha3.sha3_256;
 
 const edgeMap = {};
 
-export const random = () => crypto.randomBytes(32);
+export const random = (size = 256) => crypto.randomBytes(Math.ceil(size / 8));
+
+export const truncateByte = (value, size) => {
+  const mask = Math.pow(2, size) - 1;
+  return value & mask;
+};
+
+export const ensureBit = (value, bit) => {
+  const bitMask = Math.pow(2, bit - 1);
+  return value | bitMask;
+};
+
+export const capByte = (value, size) => {
+  const newByte = truncateByte(value, size);
+  return ensureBit(newByte, size);
+};
+
+export const markSize = (hash, size) => {
+  const firstByte = hash[0];
+
+  const markBit = ((size - 1) % 8) + 1;
+  hash[0] = capByte(firstByte, markBit);
+
+  return hash;
+};
+
+export const measureSize = hash => {
+  if(hash.length === 0)
+    return 0;
+
+  const firstByte = hash[0];
+
+  let count = 8;
+  while(count) {
+    const bitMask = Math.pow(2, count - 1);
+    if(firstByte & bitMask)
+      break;
+    count--;
+  }
+
+  return ((hash.length - 1) * 8) + count;
+};
+
+export const randomHash = (size = 256) => {
+  const hash = random(size);
+  return markSize(hash, size);
+};
 
 export const base64ToBuffer = base64 => {
   if(!base64)
@@ -22,6 +68,8 @@ export const bufferToBase64 = buffer => {
 
   return buffer.toString('base64');
 };
+
+export const readEdge = node => edgeMap[bufferToBase64(node)];
 
 export const hashList = list => {
   const hash = sha256.create();
@@ -65,5 +113,3 @@ export const deepHashEdge = (...args) => {
 
   return hashEdge(head, tail);
 };
-
-export const readEdge = node => edgeMap[bufferToBase64(node)];
